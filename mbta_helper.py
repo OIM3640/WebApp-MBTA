ec7ef7c99fc0bcb2fbfe29d74091f9f55422b966
151433x <75758020+151433x@users.noreply.github.com>
2022-04-01 20:13:41 -0400

    uploaded map function

--- mbta_helper.py
+++ mbta_helper.py
@@ -43,7 +43,7 @@ def get_lat_long(place_name):
 
 def get_nearest_station(latitude,longitude):
     """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
+    Given latitude and longitude strings, return a (station_name, wheelchair_accessible, lat, long)
     tuple for the nearest MBTA station to the given coordinates.
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
@@ -54,6 +54,8 @@ def get_nearest_station(latitude,longitude):
         mbta_pull=get_json(f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}')
         station_name=mbta_pull['data'][0]['attributes']['name']
         wheelchair_accessible=mbta_pull['data'][0]['attributes']['wheelchair_boarding']
+        lat=mbta_pull['data'][0]['attributes']['latitude']
+        long=mbta_pull['data'][0]['attributes']['longitude']
         
         if wheelchair_accessible == 2:
             wheelchair_accessible='not wheelchair accessible'
@@ -62,7 +64,7 @@ def get_nearest_station(latitude,longitude):
         else: wheelchair_accessible='wheelchair accessible'
     except IndexError:
         return None
-    return station_name,wheelchair_accessible
+    return station_name,wheelchair_accessible,lat,long
 
 
 def find_stop_near(place_name):
@@ -72,8 +74,13 @@ def find_stop_near(place_name):
     This function might use all the functions above.
     """
     lat,long=get_lat_long(place_name)
-    result=get_nearest_station(lat,long)
-    return result 
+    stop, wheelchair, lat, long=get_nearest_station(lat,long)
+    return stop, wheelchair, lat, long
+
+def map_maker(lat, long,w,h, zoom):
+    """
+    takes lat, long returns api url of map"""
+    return f"https://open.mapquestapi.com/staticmap/v5/map?key={MAPQUEST_API_KEY}&center={lat},{long}&size={w},{h}@2x&zoom={zoom}&locations={lat},{long}"
 
 
 def main():
@@ -82,7 +89,7 @@ def main():
     """
     # pprint(get_json(f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={location}'))
     # pprint(get_lat_long('boston common'))
-    lat,long=get_lat_long('time out market')
+    lat,long=get_lat_long('boston common')
     pprint(get_nearest_station(lat,long))
     print(lat,long)
 
