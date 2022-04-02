9115183ce62fa801f1ad1e2a8fe3370d98540d49
LSE2021 <65127740+LSE2021@users.noreply.github.com>
2022-04-02 16:37:11 -0400

    Not working, will check again later

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-"""
-Simple "Hello, World" application using Flask
-"""
 
-from flask import Flask
+# from crypt import methods
+from operator import methodcaller
+from flask import Flask, render_template, request
 from config import MAPQUEST, MBTA_API
+import mbta_helper
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/mbta/', methods=["GET", "POST"])
+def get_mbta():
+    if request.method == "POST":
+        location_name = request.form["location"]
+        find_stop = mbta_helper.find_stop_near()
+        return render_template("mbta-result.html", location=location_name, mbta_stop=find_stop) #replace temp with something
+
+    return render_template("mbta-form.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
