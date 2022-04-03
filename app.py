4af52000c376d9b16a5ac7073fcc8cdc5d1fad81
thealibi <81671767+thealibi@users.noreply.github.com>
2022-04-03 01:56:47 -0400

    initial commit

--- app.py
+++ app.py
@@ -2,15 +2,22 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near, get_nearest_station, get_lat_long, get_json
+from config import MAPQUEST_API_KEY, MBTA_API_KEY
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello():
-    return 'Hello World!'
+    if request.method == "POST":
+        place = request.form["location"]
+        nearest = find_stop_near(str(place))
+
+        return render_template("stop_result.html", stop = nearest[0], access = nearest[1])
+    return render_template("stop_form.html")
+
 
 
 if __name__ == '__main__':
--- mbta_helper.py
+++ mbta_helper.py
@@ -9,6 +9,8 @@ MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # A little bit of scaffolding if you want to use it
 
+from pprint import pprint
+
 
 def get_json(url):
     """
@@ -17,7 +19,15 @@ def get_json(url):
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
-    pass
+    import urllib.request
+    import json
+
+
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    # pprint(response_data)
+    return(response_data)
 
 
 def get_lat_long(place_name):
@@ -27,7 +37,19 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding API URL formatting requirements.
     """
-    pass
+    from pprint import pprint
+
+    place = place_name.replace(' ','%')
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place},MA'
+    json_data = get_json(url)
+    # lat = json_data['displayLatLng']
+    # long = ['displayLatLng']
+    
+    results = json_data['results'][0]['locations'][0]['displayLatLng']
+    # pprint(results)
+    return(results)
+
+
 
 
 def get_nearest_station(latitude, longitude):
@@ -37,8 +59,28 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
 
+    nearby = get_json(f'https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}&filter%5Bradius%5D=0.1') #took me soooo long to figure this out on my own
+    station_name_1 = nearby['data'][1]['attributes']['name']
+    accessible_1 = nearby['data'][1]['attributes']['wheelchair_boarding']
+    accessible_clean = 0
+    if accessible_1 == 1:
+        accessible_clean = 'wheelchair accessible'
+    elif accessible_1 == 0:
+        accessible_clean = 'unknown if the stop is wheelchair accessible'
+    elif accessible_1 == 2:
+        accessible_clean = 'not wheelchair accessible'
+    # vehicle_type = nearby['data'][0]['attributes']['vehicle_type']
+    # address = nearby['data'][0]['attributes']['address']
+    # city = nearby['data'][0]['attributes']['municipality']
+    station_1_info = (station_name_1,accessible_clean)
+    #2nd closest station
+    # station_name_2 = nearby['data'][1]['attributes']['name']
+    # accessible_2 = nearby['data'][1]['attributes']['wheelchair_boarding']
+    # station_2_info = (station_name_2,accessible_2)
+    return(station_1_info)
+    # print(type(station_info))
+    # print(station_info)
 
 def find_stop_near(place_name):
     """
@@ -46,14 +88,21 @@ def find_stop_near(place_name):
 
     This function might use all the functions above.
     """
-    pass
-
+    lat_long_dict = get_lat_long(place_name)
+    latitude = float(lat_long_dict['lat'])
+    longitude = float(lat_long_dict['lng'])
+    # return(latitude)
+    return(get_nearest_station(latitude, longitude))
+    # return(lat_long_dict)
 
 def main():
     """
     You can test all the functions here
     """
-    pass
+    # get_lat_long('wellesley college')
+    # get_nearest_station(42.29822,-71.26543)
+    print(find_stop_near('boston university')) #IndexError: list index out of range (fixed)
+    #('Wellesley Hills', 2)
 
 
 if __name__ == '__main__':
