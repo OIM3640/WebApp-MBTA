f939c89fea1d6e5a81c8a1d9ec475cbb73cf59c7
angelinacho0914 <97925344+angelinacho0914@users.noreply.github.com>
2022-04-04 21:46:16 -0400

    update

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 """
 Simple "Hello, World" application using Flask
 """
-
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/index/', methods = ['GET', 'POST'])
+def show_station():
+    if request.method == 'POST':
+        place_name = request.form['place']
+
+        station = find_stop_near(place_name)
+
+        return render_template('station-result.html', place = place_name, station = station)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
