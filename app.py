48227404fe42d1a21dac8adab47cc52364115b2d
enriqueu99 <98370858+enriqueu99@users.noreply.github.com>
2022-04-10 20:28:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 #User input for location stored in location variable 
 #CREATING THE CALL FORMAT FOR THE MAPQUEST API
 location = 'newyork'
-
+#testing API
 MAPQUEST_API_KEY = 'VOkvo2bQdXve8kGHbBxzvQhJDzc6lpfG'
 url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={location}'
 f = urllib.request.urlopen(url)
@@ -33,7 +33,7 @@ response_data['results'][0]['locations'][0]['latLng']
 
 
 #SECOND API, CREATING THE CALL FORMAT FOR THE MBTA API
-
+#testing API
 lat = 42.358894
 lng = -71.056742
 url1 = f"https://api-v3.mbta.com/stops?sort=distance&filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={lng}" 
@@ -46,12 +46,12 @@ response_data2 = json.loads(response_text2)
 # def homepage():
     
    
-#     return render_template("ourwebsite.html",)
+#return render_template("ourwebsite.html",)
     
 
 
 
-
+#main app route (homepage, response page )
 
 @app.route('/', methods=["GET","POST"])
 def form():
@@ -66,14 +66,14 @@ def form():
         lat = response_data['results'][0]['locations'][0]['latLng']['lat']
         lng = response_data['results'][0]['locations'][0]['latLng']['lng']
         coordinates = [lat,lng]
-        
+        #second API call
         url1 = f"https://api-v3.mbta.com/stops?sort=distance&filter%5Blatitude%5D={lat}&filter%5Blongitude%5D={lng}" 
         g = urllib.request.urlopen(url1)
         response_text2 = g.read().decode('utf-8')
         response_data2 = json.loads(response_text2)
         response_data2['data']
         neareststation = str(response_data2['data'][0]['attributes']['at_street'])
-        
+        #conditional to determine if the location is wheelchair accesible
         if response_data2['data'][0]['attributes']['wheelchair_boarding'] == 1:
             wheelchair = 'Yes'
         else: wheelchair = 'No'
