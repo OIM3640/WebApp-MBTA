782a39a79e9527a7b503c575a200c9159d5d3300
Maria Herwagen <112500028+mherwagen@users.noreply.github.com>
2022-11-06 18:58:54 -0500

    Create api_keys.py

---
+++ api_keys.py
@@ -0,0 +1,18 @@
+def map_api():
+
+    map_quest = 'mkzRo6ydiaROmfkynLzaWHPDWB3tnRZW'
+    return map_quest
+
+
+def mbta_api():
+    mbta = 'e3848735835b4472979ba8f2dfeabc46'
+    return mbta
+
+
+def main():
+    map_api()
+    mbta_api()
+
+
+if __name__ == '__main__':
+    main()
