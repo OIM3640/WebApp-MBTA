de81a4dede941f6c9563a1ded0b9d03b25b274fc
lavenderliao <112440325+lavenderliao@users.noreply.github.com>
2022-11-07 09:01:02 -0500

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -17,7 +17,17 @@ def get_json(url):
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
-    pass
+    import urllib.request
+    import json
+    from pprint import pprint
+
+    MAPQUEST_API_KEY = 'YOUR API KEY'
+
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    pprint(response_data)
 
 
 def get_lat_long(place_name):
