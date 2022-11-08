d1003df658893f6f094de119a266c75d0b9e0e67
annaz11111 <112658440+annaz11111@users.noreply.github.com>
2022-11-07 23:30:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,30 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
-
+import sqlite3
+from flask import Flask, render_template, request, abort, g
+from mbta import find_stop_near
 
 app = Flask(__name__)
 
+DATABASE = 'app.db'
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
