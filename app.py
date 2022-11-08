6779fefb8dd09e9a3d26d944ce6d014a0a3d90e6
lavenderliao <112440325+lavenderliao@users.noreply.github.com>
2022-11-07 21:26:39 -0500

    App

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/square/<number>')  # square(number)
+# @app.route('/square/<float:number>') # no need to convert
+def square(number=None):
+    if number:
+        return str(float(number) ** 2)
+    else:
+        return "You need to provide a number"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
