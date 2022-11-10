8fbf4c0877d20839db2cf85f3058456b402e39e3
Madskuz <112597537+Madskuz@users.noreply.github.com>
2022-11-09 23:22:17 -0500

    updated app

--- app.py
+++ app.py
@@ -2,15 +2,22 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, request, render_template
+from mbta_helper import get_nearest_station
+
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        place_name = request.form["name"]
+        nearest_station = get_nearest_station(place_name)
+        return render_template("index.html", city = place_name, stop = nearest_station)
+    else:
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
