938a50b1fa70733ccca6d03a274d64ed562e2860
JaredJCastro <62853522+JaredJCastro@users.noreply.github.com>
2022-11-09 19:30:39 -0500

    updated

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 from flask import Flask, render_template, request
 from mbta_helper import find_stop_near
-
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-
-@app.route("/mbta_helper", methods= ["GET", "POST"]) 
+@app.route("/", methods= ["GET", "POST"]) 
 def nearest():
     """
     Returns nearest MBTA stop and wheelchair accessibility 
     """
     if request.method == "POST":
         place = request.form["place"]
-        stop, wheelchair_boarding = find_stop_near(place)
+        if IndexError:
+            return render_template("error.html")
+        else:
+            stop, wheelchair_boarding = find_stop_near(place)
 
-        return render_template(
-            "results.html", stop=stop, wheelchair_boarding=wheelchair_boarding
+            return render_template("results.html", stop=stop, wheelchair_boarding=wheelchair_boarding
         )
     else:
         return render_template ("index.html", error=True)
 
-
+# @app.errorhandler(HTTPException)
+# def error_page():
+#     return render_template ("error.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
