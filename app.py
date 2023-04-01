24f08430a5ddcf8ad4deebd3eb335dbab9933286
matthewsyrigos <122997452+matthewsyrigos@users.noreply.github.com>
2023-04-01 16:30:10 -0400

    Update 2 (app.py)

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import main
 
+import ssl
+import urllib.request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def mbta_helper():
+    return render_template('index.html')
 
+@app.get('/input/')
+def get_location():
+    return render_template('index.html')
+
+@app.post('/input/')
+def post_location():
+    place_name = request.form['place']
+    return render_template('index.html', place=place_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
