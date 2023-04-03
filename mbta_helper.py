faabd41c3c30935d06101f2b43094aec76bb014e
aaliceqi <aqi1@babson.edu>
2023-04-03 01:06:13 -0400

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -10,22 +10,26 @@ MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 
+# print(url) # Try this URL in your browser first
 # A little bit of scaffolding if you want to use it
 
 
-def get_json(url: str) -> dict:
+def get_json(query) -> dict:
     """
     Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
     
+    url=f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
+    print(url)
     f = urllib.request.urlopen(url)
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
-    pprint.pprint(response_data)
+    return response_data
 
-    pass
+
+    
 
 
 def get_lat_long(place_name: str) -> tuple[str, str]:
@@ -34,8 +38,15 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
 
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
-    pass
+    query= "Babson%20College"
+    response_data = get_json(query)
+    length= len(response_data["features"])
+    for i in range(length):
+        if place_name in response_data["features"][i]['place_name']:
+            location = response_data["features"][i]["geometry"]["coordinates"]
+            return tuple(location)
 
+ 
 
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
@@ -59,8 +70,12 @@ def main():
     """
     You can test all the functions here
     """
-    get_json(MAPBOX_BASE_URL)
-    pass
+
+    place_name = "231 Forest St"
+    # pprint.pprint(get_json(place_name))
+    print(get_lat_long(place_name))
+
+    
 
 
 if __name__ == '__main__':
