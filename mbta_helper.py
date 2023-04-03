dd76282527b329e602d38d6215c69e52ccf2197d
MSERG01 <savilabermudez1@babson.edu>
2023-04-03 11:50:58 -0400

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -22,6 +22,10 @@ def get_json(url: str) -> dict:
     query = 'Babson%20College'
     url = f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
     print(url)  # Try this URL in your browser first
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        response_data = json.loads(response_text)
+        pprint(response_data)
 
 
 get_json(MAPBOX_BASE_URL)
