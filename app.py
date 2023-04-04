33535ba7afb74cd35be53c0137b6edf597b56750
xzzz7 <xng1@babson.edu>
2023-04-04 12:11:44 -0400

    toodles

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template("index.html") 
-    #there should also be an form here for user to enter input of location 
 
 #this page will show the results of the user's search input 
 @app.route('/nearest_mbta', methods = ['POST'])
