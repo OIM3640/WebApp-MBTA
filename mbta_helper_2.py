dcb98634d2ae7063f3879c154fb1b198305d690b
MSERG01 <savilabermudez1@babson.edu>
2023-04-04 14:35:10 -0400

    Updated Backend Function MBTA

--- mbta_helper_2.py
+++ mbta_helper_2.py
@@ -2,8 +2,11 @@
 from config import MAPBOX_TOKEN, MBTA_API_KEY
 import urllib.request
 import json
-import requests
 import pprint
+
+# Date time for arrival information
+import requests
+from datetime import datetime
 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
@@ -42,29 +45,61 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
     """
     url = get_url(place_name)
     response_data = get_json(url)
-    latitude, longitude = response_data['features'][0]['center']
-    return latitude, longitude
+    longitude, latitude = response_data['features'][0]['center']
+    latitude = round(latitude, 4)
+    longitude = round(longitude, 4)
+    return longitude, latitude
 
 
-def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
+def get_predictions(station_id: str) -> str:
+    '''
+    Given station ID, return the predicted arrival time of next vehicle
+
+    '''
+    url = f"https://api-v3.mbta.com/predictions?filter%5Bstop%5D={station_id}&sort=arrival_time&direction_id=0&api_key={MBTA_API_KEY}"
+    response_data = get_json(url)
+
+    if response_data['data']:
+        # Get predicted arrival time
+        next_arrival_time = response_data['data'][0]['attributes']['arrival_time']
+        # convert arrival time to python datetime object
+        arrival_time = datetime.fromisoformat(next_arrival_time)
+        # Get current time
+        now = datetime.now()
+        # calc time till arrival
+        time_until_arrival = arrival_time - now
+        # return the time in minutes
+        return time_until_arrival.total_seconds()//60
+    else:
+        return "No ETA Prediction available"
+
+
+def get_nearest_station(longitude: str, latitude: str) -> tuple[str, bool]:
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible) tuple for the nearest MBTA station to the given coordinates.
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
+
+    wheelchair_code = {}
+    vehicle_code = {}
+
     """
     url = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
     response_data = get_json(url)
     if response_data['data']:
         first_stop = response_data['data'][0]
-        station_name, wheelchair_accessible = first_stop['attributes'][
-            'description'], first_stop['attributes']['wheelchair_boarding']
+        pprint.pprint(first_stop)
+        station_name, wheelchair_accessible, vehicle_type = first_stop['attributes'][
+            'name'], first_stop['attributes']['wheelchair_boarding'], first_stop['attributes']['vehicle_type']
         if wheelchair_accessible == 1:
             wheelchair_accessible = True
         else:
             wheelchair_accessible = False
-        return station_name, wheelchair_accessible
+        station_id = first_stop['id']
+        time_until_arrival = get_predictions(station_id)
+        return station_name, wheelchair_accessible, vehicle_type, time_until_arrival
     else:
-        return f"There is no stop nearby ({latitude},{longitude}), please choose another location"
+        return f"There is no stop nearby ({longitude}, {latitude}), please choose another location"
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -83,14 +118,13 @@ def main():
     """
     # url = get_url("Babson College")
     # pprint.pprint(get_json(url))
-    location = "Boston Commons"
+    location = "Old North Church"
+    print(location)
     print(get_lat_long(location))
     longitude, latitude = get_lat_long(location)
-    print(get_nearest_station(latitude, longitude))
-
-    print(find_stop_near(location))
-
-    #  0 = No data, 1 = Accessible, 2 = Not Accessible
+    print(get_nearest_station(longitude, latitude))
+    # print(get_nearest_station(get_lat_long(location)))
+    # print(find_stop_near(location))
 
 
 if __name__ == '__main__':
