cbfe09581281c50066589cd0c765a5bc58259ef9
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-04 09:06:59 -0400

    wip to make site functional

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+from mbta_helper import find_stop_near
 from flask import Flask, render_template, request
-from mbta_helper import main
-
-import ssl
-import urllib.request
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
 
 @app.route('/nearest', methods=['POST'])
 def nearest_station():
     # use place_name to calcuate the nearest MBTA station
     place_name = request.form['place_name']
+    result = find_stop_near(place_name)
 
     # Retrun result to result.html template
-    return render_template('result.html', station=nearest_station)
+    return render_template('result.html', place_name=place_name, result=result)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
