fba9ca6a80bf5c48f900e6520b514ebed2451eed
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-04 17:53:16 -0400

    minor changes

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.get('/')
 def home_page():
     return render_template('index.html')
 
 
-@app.route('/nearest', methods=['POST'])
+@app.post('/nearest')
 def nearest_station():
     # use place_name to calcuate the nearest MBTA station
-    place_name = request.form['place_name']
-    result = find_stop_near(place_name)
+    place_name = request.form.get('place_name')
+    stop = find_stop_near(place_name)
 
     # Retrun result to result.html template
-    return render_template('result.html', place_name=place_name, result=result)
+    return render_template('result.html', place_name=place_name, nearest_stop=stop)
 
 
 if __name__ == '__main__':
