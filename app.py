9f962a084086ced9c596bb5f2c8337a7ea4eab12
amandalxllll <xliu7@babson.edu>
2023-04-04 22:13:02 -0400

    edit

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template, request
 from mbta_helper import find_stop_near
+from getweather import get_temp
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def show_station():
     return render_template("index.html")
+
 
 @app.post("/nearest_mbta")
 def station_post():
     try:
         place_name = request.form.get("place")
         (station, wheelchair) = find_stop_near(place_name)
-        return render_template("station-result.html", place = place_name, station = station, wheelchair = wheelchair)
+        return render_template(
+            "station-result.html",
+            place=place_name,
+            station=station,
+            wheelchair=wheelchair,
+        )
     except:
         return render_template("error.html")
 
-@app.route("/weather/")  #Amanda
-def weather():
-    return render_template("weather.html")
 
-if __name__ == '__main__':
+@app.route("/weather/")  # Amanda
+def weather():
+    try:
+        place_name = request.form.get("place")
+        temperature = get_temp(place_name.replace(" ", "%20"))
+        return render_template(
+            "station-result.html", place=place_name, temp=temperature
+        )
+    except:
+        return render_template("error.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ getweather.py
@@ -0,0 +1,28 @@
+import urllib.request
+import json
+import pprint
+from config import OPENWEATHERMAP_APIKEY
+
+
+
+
+def get_temp(place: str) -> float:
+    """
+    return the current temperature of a given city
+    """
+    place = place.replace(' ', '%20')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={place},us&APPID={OPENWEATHERMAP_APIKEY}&units=metric'
+    print(url)
+
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        # print(response_text)
+        response_data = json.loads(response_text)
+    # pprint.pprint(response_data)
+    return response_data['main']['temp']
+
+def main ():
+    print(get_temp('wellesley'))
+
+if __name__ == "__main__":
+    main()
