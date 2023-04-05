2d832e845b60eccd97da4eca79c186835180ebaa
matthewsyrigos <122997452+matthewsyrigos@users.noreply.github.com>
2023-04-05 11:01:00 -0400

    Both Map and Weather Functions Work!

--- app.py
+++ app.py
@@ -9,29 +9,39 @@ def home_page():
     return render_template('index.html')
 
 
-@app.post('/nearest')
-def nearest_station():
-    # use place_name to calcuate the nearest MBTA station
+# @app.post('/nearest')
+# def nearest_station():
+#     # use place_name to calcuate the nearest MBTA station
+#     place_name = request.form.get('place_name')
+#     stop, wheelchair = find_stop_near(place_name)
+
+#     # Retrun result to result.html template
+#     return render_template('result.html', place_name=place_name, nearest_stop=stop, wheelchair_accessible=wheelchair)
+
+
+# @app.post('/map')
+# def show_map():
+#     place_name = request.form.get('place_name')
+#     lat, lng = get_lat_long(place_name)
+#     html_map = f'<iframe src="https://maps.google.com/maps?q={lat},{lng}&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe>'
+#     return render_template('result.html', place_name=place_name, map=html_map)
+
+
+# @app.post('/weather')
+# def show_weather():
+#     place_name = request.form.get('place_name')
+#     temperature = weather(place_name)
+#     return render_template('result.html', place_name=place_name, temperature=temperature)
+
+@app.post('/results')
+def results():
     place_name = request.form.get('place_name')
     stop, wheelchair = find_stop_near(place_name)
-
-    # Retrun result to result.html template
-    return render_template('result.html', place_name=place_name, nearest_stop=stop, wheelchair_accessible=wheelchair)
-
-
-@app.post('/map')
-def show_map():
-    place_name = request.form.get('place_name')
     lat, lng = get_lat_long(place_name)
     html_map = f'<iframe src="https://maps.google.com/maps?q={lat},{lng}&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe>'
-    return render_template('result.html', place_name=place_name, map=html_map)
-
-
-@app.post('/weather')
-def show_weather():
-    place_name = request.form.get('place_name')
     temperature = weather(place_name)
-    return render_template('result.html', place_name=place_name, weather=temperature)
+    return render_template('result.html', place_name=place_name, nearest_stop=stop, wheelchair_accessible=wheelchair, map=html_map, temperature=temperature)
+
 
 
 if __name__ == '__main__':
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,3 +1,7 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
 # Your API KEYS (you need to use your own keys - very long random characters)
 from pprint import pprint
 import json
