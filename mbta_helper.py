cf9984f0b32400f93e64598eab9cca1a7d8d386e
jrobertson1414 <jrobertson1@babson.edu>
2023-04-05 15:28:28 -0400

    get_nearest_station code complete

--- mbta_helper.py
+++ mbta_helper.py
@@ -18,7 +18,6 @@ MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 def get_json(url: str) -> dict:
     """
     Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
-
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
     with urllib.request.urlopen(url) as f:
@@ -30,7 +29,6 @@ def get_json(url: str) -> dict:
 def get_lat_long(place_name: str) -> tuple[str, str]:
     """
     Given a place name or address, return a (latitude, longitude) tuple with the coordinates of the given place.
-
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
     mapbox_url = f'{MAPBOX_BASE_URL}/{place_name}.json?access_token={MAPBOX_TOKEN}&types=poi'
@@ -44,16 +42,22 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible) tuple for the nearest MBTA station to the given coordinates.
-
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    pass
+
+    MBTA_url = f"{MBTA_BASE_URL}?filter[route_type]=0&include=wheelchair_boarding&fields[stop]=name,latitude,longitude,wheelchair_boarding&sort=distance&filter[latitude]={latitude}&filter[longitude]={longitude}&api_key={MBTA_API_KEY}"
+    response_data = get_json(MBTA_url)
+    nearest_station = response_data["data"][0]
+    station_name = nearest_station ["attributes"]["name"]
+    wheelchair_accessible = nearest_station["attributes"]["wheelchair_boarding"] == "1"
+
+    return station_name, wheelchair_accessible
+
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
-
     This function might use all the functions above.
     """
     pass
@@ -64,6 +68,7 @@ def main():
     You can test all the functions here
     """
     print(get_lat_long('Wellesley'))
+    print(get_nearest_station("-71.30712925","42.293797"))
     pass
 
 
