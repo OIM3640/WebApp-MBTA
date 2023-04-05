51a95d05e1f0b330c1b5b4b5f7032ec15c3053c1
cc4987 <98005317+cc4987@users.noreply.github.com>
2023-04-04 22:22:16 -0400

    fixed w new function

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ from mbta_helper_draft import get_station
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-
-@app.get('/mbta')
-def mbta_get():
+@app.route('/', methods=['GET', 'POST'])
+def mbta_form():
+    if request.method == 'POST':
+        location_name = request.form.get('query')
+        station = get_station(location_name.replace(' ', '%20'))
+        return f'the closest station to {location_name} is {station}'
     return render_template('index.html')
 
-@app.post('/mbta')
-def mbta_post():
-    location_name = request.form.get('query')
-    station = get_station(location_name)
-    return render_template('index.html', query = location_name, stop_name = station)
+
+# @app.get('/mbta')
+# def mbta_get():
+#     return render_template('index.html')
+
+# @app.post('/mbta')
+# def mbta_post():
+#     location_name = request.form.get('query')
+#     station = get_station(location_name.replace(' ', '%20'))
+#     return render_template('index.html', city = location_name, stop_name = station)
 
 
 
