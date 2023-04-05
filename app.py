ccccc9a7246e61d7a76daeed57919ce31b222048
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-04 21:15:45 -0400

    add map

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from mbta_helper import find_stop_near
+from mbta_helper import find_stop_near, get_nearest_station
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -17,6 +17,13 @@ def nearest_station():
 
     # Retrun result to result.html template
     return render_template('result.html', place_name=place_name, nearest_stop=stop, wheelchair_accessible=wheelchair)
+
+
+@app.route('/map/')
+def show_map():
+    lat, lng = get_nearest_station()
+    html_map = f'<iframe src="https://maps.google.com/maps?q={lat},{lng}&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe>'
+    return html_map
 
 
 if __name__ == '__main__':
