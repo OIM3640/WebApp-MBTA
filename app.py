4b0fa729afefff0dc4919397212b40d5675dcdd8
aaliceqi <aqi1@babson.edu>
2023-04-05 22:14:48 -0400

    update

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ from mbta_helper import mbta_coord
 app = Flask(__name__)
 
 
-@app.route('/mbta', methods=['GET', 'POST'])
-def mbta_form():
-    if request.method == 'POST':
-        location_name = request.form.get('query')
-        station = get_station(location_name.replace(' ', '%20'))
-        return f'The closest station to {location_name} is stop {station[0]} on {station[2]} and it is {station[1]}.'
+# @app.route('/mbta', methods=['GET', 'POST'])
+@app.get('/mbta')
+def mbta_get():
     return render_template('index.html')
+@app.post('/mbta')
+def mbta_post():
+    location_name = request.form.get('query')
+    station = get_station(location_name.replace(' ', '%20'))
+    lat, lng = mbta_coord(location_name.replace(' ','%20'))
+    html_map = f'<iframe src="https://maps.google.com/maps?q={lat},{lng}&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=&amp;output=embed"<</iframe>'
+    print(html_map)
+    return render_template('result.html', location_name = location_name, station_0 = station[0], station_1 = station[1], station_2 = station[2], html_map = html_map)    
 
 
 if __name__ == '__main__':
