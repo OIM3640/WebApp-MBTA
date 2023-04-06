b68bcb6b9fe0f9a4922b04a5158d0ff4913278c6
msy011015 <122825559+msy011015@users.noreply.github.com>
2023-04-05 21:44:10 -0400

    finished

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/MBTA/', methods=["GET", "POST"])
+def MBTA():
+    if request.method == "POST":
+        place_name = str(request.form["place"])
+        results = find_stop_near(place_name)
+        if results:
+            return render_template("result.html",  results=results)
+        else:
+            return render_template("form.html", error=True)
+    return render_template("form.html", error=None)
 
 
 if __name__ == '__main__':
