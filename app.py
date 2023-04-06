803c0213c6d59917c32f6a3ee36104b2db93ccc3
angelawong123 <cwong3@babson.edu>
2023-04-06 10:18:06 -0400

    format

--- app.py
+++ app.py
@@ -4,37 +4,44 @@ from mbta_helper import get_weather
 
 app = Flask(__name__)
 
-@app.get('/station/')
-def station_get():
-    return render_template('station-form.html', error=None)
 
-@app.post('/station/')
+@app.get("/station/")
+def station_get():
+    return render_template("station-form.html", error=None)
+
+
+@app.post("/station/")
 def station_post():
     place_name = str(request.form["place_name"])
-    route_type = str(request.form['route_type'])
+    route_type = str(request.form["route_type"])
     station, wheelchair_accessible = find_stop_near(place_name, route_type)
     weather, temperature = get_weather(place_name)
 
     if wheelchair_accessible == 0:
-        wheelchair = 'No Information'
+        wheelchair = "No Information"
     elif wheelchair_accessible == 1:
-        wheelchair = 'Accessible'
+        wheelchair = "Accessible"
     else:
-        wheelchair = 'Inaccessible'
-        
+        wheelchair = "Inaccessible"
+
     if station:
-        return render_template('station-result.html', place=place_name, station=station, wheelchair=wheelchair, route=route_type, weather = weather, temperature=temperature)
+        return render_template(
+            "station-result.html",
+            place=place_name,
+            station=station,
+            wheelchair=wheelchair,
+            route=route_type,
+            weather=weather,
+            temperature=temperature,
+        )
     else:
-        return render_template('station-form.html', error=True)
+        return render_template("station-form.html", error=True)
 
 
-
-
-
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello World!'
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- mbta_helper.py
+++ mbta_helper.py
@@ -11,7 +11,7 @@ MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 MAPBOX_TOKEN = "pk.eyJ1IjoiYW5nZWxhd29uZzEyMyIsImEiOiJjbGZ2b2FyM28wOTh6M25wZGJhcW04ODZpIn0.lT5TeXhQjnwH3fzWq8mjAA"
 MBTA_API_KEY = "b0f1e154e91042b5a636d8c6663e88ac"
-WEATHER_API_KEY = '5f4ccf8ce381abfe7e6db22d4f1079a9'
+WEATHER_API_KEY = "5f4ccf8ce381abfe7e6db22d4f1079a9"
 # A little bit of scaffolding if you want to use it
 
 
@@ -39,14 +39,14 @@ def get_lat_long(address: str) -> tuple[str, str]:
 
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
-    # Replace entered spaces in address with %20 
+    # Replace entered spaces in address with %20
     address = address.replace(" ", "%20")
 
-    # Generate API url with get_url function and get the json data 
+    # Generate API url with get_url function and get the json data
     url = get_url(address)
     json_data = get_json(url)
 
-    # Check if the json_data contains any data, and returns the longitude and latitude of the given address. If not, returns no result. 
+    # Check if the json_data contains any data, and returns the longitude and latitude of the given address. If not, returns no result.
     if len(json_data["features"]) > 0:
         longitude, latitude = json_data["features"][0]["center"]
         return longitude, latitude
@@ -54,38 +54,42 @@ def get_lat_long(address: str) -> tuple[str, str]:
         print(f"No results found for place: {address}")
 
 
-def get_nearest_station(longitude: str, latitude: str, route_type: str) -> tuple[str, bool]:
+def get_nearest_station(
+    longitude: str, latitude: str, route_type: str
+) -> tuple[str, bool]:
     """
     Given latitude and longitude strings, return a (station_name, wheelchair_accessible) tuple for the nearest MBTA station to the given coordinates.
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
     MBTA_STOPS_API_URL = "https://api-v3.mbta.com/stops"
-    # Possible route types 
+    # Possible route types
     route_types = {
-        'all': ' ',
-        'light rail': '0',
-        'heavy rail': '1',
-        'commuter rail': '2',
-        'bus': '3',
-        'ferry': '4'
+        "all": " ",
+        "light rail": "0",
+        "heavy rail": "1",
+        "commuter rail": "2",
+        "bus": "3",
+        "ferry": "4",
     }
-    # Parameters for API 
+    # Parameters for API
     params = {
         "filter[latitude]": latitude,
         "filter[longitude]": longitude,
         "sort": "distance",
         "api_key": MBTA_API_KEY,
-         "filter[route_type]":route_types[route_type]
+        "filter[route_type]": route_types[route_type],
     }
     # Get JSON data from API and store in station_data
     response = requests.get(MBTA_STOPS_API_URL, params=params)
     station_data = response.json()
 
     # If station_data contains data, extract the station name and whether it is wheelcahir accesible. If not, it will return None, which means that there are no MBTA stops close enough to the given place.
-    if station_data['data']:
+    if station_data["data"]:
         station_name = station_data["data"][0]["attributes"]["name"]
-        wheelchair_accessible = station_data["data"][0]["attributes"]["wheelchair_boarding"]
+        wheelchair_accessible = station_data["data"][0]["attributes"][
+            "wheelchair_boarding"
+        ]
         return station_name, wheelchair_accessible
     else:
         print(f"No stops close enough.")
@@ -100,15 +104,16 @@ def find_stop_near(place_name: str, route_type: str) -> tuple[str, bool]:
     """
     place_name = place_name.replace(" ", "%20")
 
-    # Get longitude and latitude for the given place name 
+    # Get longitude and latitude for the given place name
     lat, lon = get_lat_long(place_name)
-    # Get the nearest station and whether it is wheelchair accessible using the longitude and latitude from above. 
+    # Get the nearest station and whether it is wheelchair accessible using the longitude and latitude from above.
     nearest_station, wheelchair_accessible = get_nearest_station(lat, lon, route_type)
     return nearest_station, wheelchair_accessible
 
+
 def get_weather(place_name):
     """
-    Given a place name or address, returns the current weather and temperature. 
+    Given a place name or address, returns the current weather and temperature.
     """
     place_name = place_name.replace(" ", "%20")
     lon, lat = get_lat_long(place_name)
@@ -117,7 +122,7 @@ def get_weather(place_name):
     weather_data = get_json(url)
     # Get the current temperature and weather
     temperature = weather_data["main"]["temp"]
-    weather = weather_data['weather'][0]['main']
+    weather = weather_data["weather"][0]["main"]
     return weather, temperature
 
 
@@ -134,6 +139,7 @@ def main():
     # lon, lat = get_lat_long(place_name)
     # print(get_weather(place_name))
     # print(find_stop_near(place_name, route_type))
+
 
 if __name__ == "__main__":
     main()
