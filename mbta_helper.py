7e9186bbb2c1c8293389cc2f4e66f64cf37b5adf
ronaldliujr <rliujr1@babson.edu>
2023-11-08 11:14:05 -0500

    Update mbta_helper.py

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,6 +1,8 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
 from config import MAPBOX_TOKEN, MBTA_API_KEY
 
+# mapbox key - pk.eyJ1Ijoicm9uYWxkbGl1anIiLCJhIjoiY2xvcHlra3I1MGFxaTJrbG52djMzc3k0MyJ9.Uixk2uqshXS9RYtV5RD3fg
+# mbta key - 1b3af775c6e94d48814fb0ca3ed91a61
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
@@ -51,5 +53,5 @@ def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
