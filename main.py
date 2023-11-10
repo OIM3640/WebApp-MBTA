f0fa39b9e93aa1ca6eb0147cce113a987ce6fdb1
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-11-09 23:37:11 -0500

    Push 2 - Needs front-end and integration

---
+++ main.py
@@ -0,0 +1,12 @@
+from mapboxdemo import get_coords
+from mbtademo import get_stations, count_stations, top_x
+
+if __name__ == '__main__':
+    get_coords()
+    get_stations()
+    count_stations()
+    top_x()
+    print()
+
+
+
