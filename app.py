7ae03f19aff363bf4e804c6c53fbedd3b6cebc78
ronaldliujr <rliujr1@babson.edu>
2023-11-13 22:13:45 -0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near, get_temp, get_lat_long
 
 app = Flask(__name__)
 
@@ -7,6 +7,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/station/", methods=["GET", "POST"])
+def find_mbta():
+    if request.method == "POST":
+        place_name = request.form["place_name"]
+
+        # station information
+        station = find_stop_near(place_name)
+
+        # weather information
+        latitude, longitude = get_lat_long(place_name)
+        temperature = get_temp(latitude, longitude)
+
+        return f"{station}\n The current temperature of is {temperature[0]}°C, with the low being {temperature[1]}°C and the high being {temperature[2]}°C."
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,5 +1,5 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
-from config import MAPBOX_TOKEN, MBTA_API_KEY
+from config import MAPBOX_TOKEN, MBTA_API_KEY, OPENWEATHERMAP_APIKEY
 import json
 import pprint
 import urllib.request
@@ -17,7 +17,7 @@ def get_json(url: str) -> dict:
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
     with urllib.request.urlopen(url) as f:
-        response_text = f.read().decode('utf-8')
+        response_text = f.read().decode("utf-8")
         response_data = json.loads(response_text)
         # pprint.pprint(response_data)
         return response_data
@@ -30,13 +30,16 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
     query = place_name
-    query = query.replace(' ', '%20') # In URL encoding, spaces are typically replaced with "%20"
-    url=f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
+    query = query.replace(
+        " ", "%20"
+    )  # In URL encoding, spaces are typically replaced with "%20"
+    url = f"{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi"
     response_data = get_json(url)
-    
-    latitude = response_data['features'][0]['geometry']['coordinates'][1]
-    longitude = response_data['features'][0]['geometry']['coordinates'][0]
+
+    latitude = response_data["features"][0]["geometry"]["coordinates"][1]
+    longitude = response_data["features"][0]["geometry"]["coordinates"][0]
     return latitude, longitude
+
 
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
@@ -47,11 +50,12 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     url2 = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
     response_data = get_json(url2)
     # print(response_data)
-    station_name = response_data['data'][0]['attributes']['name']
-    wheelchair_accessible = response_data['data'][0]['attributes']['wheelchair_boarding'] == 1
-    
+    station_name = response_data["data"][0]["attributes"]["name"]
+    wheelchair_accessible = (
+        response_data["data"][0]["attributes"]["wheelchair_boarding"] == 1
+    )
+
     return station_name, wheelchair_accessible
-    
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -61,21 +65,39 @@ def find_stop_near(place_name: str) -> tuple[str, bool]:
     This function might use all the functions above.
     """
     latitude, longitude = get_lat_long(place_name)
-    
     station_name, wheelchair_accessible = get_nearest_station(latitude, longitude)
-
     wheelchair_accessible = str(wheelchair_accessible).lower()
-    
-    print(f"The nearest Station to {place_name} is {station_name}. It is {wheelchair_accessible} that it is wheelchair accessible.")
+
+    result = f"The nearest station to {place_name} is {station_name}. It is {wheelchair_accessible} that it is wheelchair accessible."
+
+    return result
+
+
+def get_temp(latitude, longitude):
+    APIKEY = OPENWEATHERMAP_APIKEY
+    country_code = "us"
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&APPID={APIKEY}&units=metric"
+
+    response_data = get_json(weather_url)
+    temp = response_data["main"]["temp"]
+    temp_min = response_data["main"]["temp_min"]
+    temp_max = response_data["main"]["temp_max"]
+
+    return temp, temp_min, temp_max
 
 
 def main():
     """
     You should test all the above functions here
     """
-    find_stop_near("Boston College")
-    # latitude, longitude = get_lat_long("Boston College")
-    # get_nearest_station(latitude,longitude)
+    place_name = "Boston College"
+    latitude, longitude = get_lat_long(place_name)
+    result = find_stop_near(place_name)
+    weather_result = get_temp(latitude, longitude)
+
+    print(f"Latitude: {latitude}, Longitude: {longitude}")
+    print(result)
+    print(get_temp(latitude, longitude))
 
 
 if __name__ == "__main__":
