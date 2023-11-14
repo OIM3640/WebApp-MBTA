d3481d4f3f7e099bab457994f8fbaadb37a0268a
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-11-14 18:33:47 -0500

    App.py - Flask, Program.py - Functionality, Main.py - Test code, Templates - HTML

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from program import your_closest_station
 import pprint as p
 
 if __name__ == '__main__':
+    """
+    Runs main function with a sample query and is called by app
+    """
     p.pprint(your_closest_station('Fenway Park'))
 
 
