8319697563c6e9eabca65f4601ac6576214d6cb7
Maria Dominguez <mdominguezgonzalez1@babson.edu>
2023-11-14 15:26:05 -0500

    templates inicial

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import mbta_helper
 
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
     return 'Hello World!'
+
+def home():
+    if request.method == 'POST':
+        address = request.form.get('address')
+        mbta_helper.main(address)
+    return render_template('index.html', message='Hello, World!')
 
 
 if __name__ == '__main__':
