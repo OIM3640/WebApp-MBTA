c68aaa4ef6a95ef86a4e3c0036f51915843ff5c5
Immanuel-Ntim-Addae <intimaddae1@babson.edu>
2023-11-14 18:08:21 -0500

    Update

--- app_final.py
+++ app_final.py
@@ -1,6 +1,5 @@
 from flask import Flask , render_template , request
-from edits import find_stop_near
-
+from mbta_helper_final import find_stop_near
 app = Flask(__name__)
 
 
@@ -8,19 +7,19 @@ app = Flask(__name__)
 def mbta_finder(name=None):
     return render_template('index.html', name=name) #functioning as intended.
 
-#Based off code found on stack overflow link:https://stackoverflow.com/questions/43677564/passing-input-from-html-to-python-and-back
 @app.post('/')
 def hello():
         user_location_input = request.form['user_location_input']
         answer_1, answer_2 = find_stop_near(user_location_input)
-        #This is an experiment
         #split answer into two variables and use them to say where the person is. 
-        return render_template('results.html',
-                        answer_1 = answer_1,
-                        answer_2 = answer_2,
-                        user_location_input = user_location_input
-                        )
-        #return f"{answer}"
+        return render_template('sucessful_query_results.html',
+                    answer_1 = answer_1,
+                    answer_2 = answer_2,
+                    user_location_input = user_location_input
+                    )
+        
+             
+
 
 if __name__ == '__main__':
     app.run(debug=True)
