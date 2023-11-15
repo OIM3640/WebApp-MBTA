c4c9d8afda671732425f76a28f0cea342b359bef
VickyYan0822 <143618285+VickyYan0822@users.noreply.github.com>
2023-11-14 22:46:04 -0500

    update

---
+++ weather.py
@@ -0,0 +1,22 @@
+import urllib.request
+import json
+from config import WEATHER_API_KEY
+from mbta_helper import get_lat_long
+
+
+def get_temp(place_name):
+    APIKEY = WEATHER_API_KEY
+    latitude, longtitude = get_lat_long(place_name)
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longtitude}&appid={APIKEY}'
+    print(url)
+
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        print(response_text)
+        response_data = json.loads(response_text)
+        weather =  response_data['main']['temp']
+        weather = weather - 273.15
+        return weather
+
+
+print(get_temp("Boston Common"))
