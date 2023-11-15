08fdddcad4c335b6641f32010420207844f21660
swuuu1 <swu1@babson.edu>
2023-11-14 23:35:07 -0500

    Complete MBTA App

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+import mbta_helper
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.get("/")
+def mbta_get():
+    return render_template("index.html")
 
+@app.post("/")
+def mbta_post():
+    location = request.form['location']
+    nearest_station,wheelchair_accessible = mbta_helper.find_stop_near(location)
+    if wheelchair_accessible:
+        return f"A station near {location} is {nearest_station}, and it is wheelchair accessible"
+    return f"A station near {location} is {nearest_station}, and it is not wheelchair accessible"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,11 +1,21 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
 from config import MAPBOX_TOKEN, MBTA_API_KEY
 
+import pprint
+import json
+import urllib.request
+import urllib.parse
+
+MAPBOX_TOKEN
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
+query = 'Babson College'
+query = query.replace(' ', '%20') # In URL encoding, spaces are typically replaced with "%20"
+url=f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
+# print(url) # Try this URL in your browser first
 
 # A little bit of scaffolding if you want to use it
 def get_json(url: str) -> dict:
@@ -14,8 +24,12 @@ def get_json(url: str) -> dict:
 
     Both get_lat_long() and get_nearest_station() might need to use this function.
     """
-    pass
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        response_data = json.loads(response_text)
+        return response_data
 
+# pprint.pprint(get_json(url))
 
 def get_lat_long(place_name: str) -> tuple[str, str]:
     """
@@ -23,8 +37,16 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
 
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
-    pass
+    # get mapbox dictionary
+    query = place_name
+    query = query.replace(' ', '%20') # In URL encoding, spaces are typically replaced with "%20"
+    url = f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
+    mapbox_dict = get_json(url)
+    lat, long = mapbox_dict['features'][0]['center']
 
+    return lat, long
+
+# print(get_lat_long('Babson College'))
 
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
@@ -32,7 +54,19 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    pass
+    sorting = 'distance'
+    params = urllib.parse.urlencode({'filter[latitude]': longitude, 'filter[longitude]': latitude, 'page[limit]': 1, 'sort': sorting})
+    url = '?'.join((MBTA_BASE_URL,params))
+    
+    nearest_station = get_json(url)
+    station_name = nearest_station['data'][0]['attributes']['name']
+    wheelchair_value = nearest_station['data'][0]['attributes']['wheelchair_boarding']
+    if wheelchair_value == 1:
+        wheelchair_accessible = True
+    else: 
+        wheelchair_accessible = False
+    return station_name, wheelchair_accessible
+
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -41,14 +75,16 @@ def find_stop_near(place_name: str) -> tuple[str, bool]:
 
     This function might use all the functions above.
     """
-    pass
+    lat, long = get_lat_long(place_name)
+    return get_nearest_station(lat, long)
+
 
 
 def main():
     """
     You should test all the above functions here
     """
-    pass
+    print(find_stop_near('Boston Common'))
 
 
 if __name__ == '__main__':
