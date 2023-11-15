25a998e228f95e11fdd63450c12ba3163b597630
vincent-dinatale <122955042+vincent-dinatale@users.noreply.github.com>
2023-11-14 22:42:11 -0500

    Work in Progress

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,22 +1,40 @@
 # Your API KEYS (you need to use your own keys - very long random characters)
-from config import MAPBOX_TOKEN, MBTA_API_KEY
+from config import MAPBOX_TOKEN, MBTA_API_KEY, WEATHER_APIKEY, TICKET_KEY
 
-#Hello
+import json
+import pprint
+import urllib.request
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
-
+   
+def url_encode(place_name: str) -> str:
+    """
+    
+    """
+    place_name = place_name.replace(' ', '%20')
+    url = f'{MAPBOX_BASE_URL}/{place_name}.json?access_token={MAPBOX_TOKEN}&types=poi'
+    return url
 
 # A little bit of scaffolding if you want to use it
-def get_json(url: str) -> dict:
-    """
-    Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
+# def get_json(url: str) -> dict:
+#     """
+#     Given a properly formatted URL for a JSON web API request, return a Python JSON object containing the response to that request.
 
-    Both get_lat_long() and get_nearest_station() might need to use this function.
-    """
-    pass
+#     Both get_lat_long() and get_nearest_station() might need to use this function.
+#     """
+#     query = 'Babson College'
+#     query = query.replace(' ', '%20') # In URL encoding, spaces are typically replaced with "%20"
+#     url=f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&types=poi'
+#     print(url) # Try this URL in your browser first
 
+#     with urllib.request.urlopen(url) as f:
+#         response_text = f.read().decode('utf-8')
+#         response_data = json.loads(response_text)
+#         pprint.pprint(response_data)
+    
+#     print(response_data['features'][0]['properties']['address'])
 
 def get_lat_long(place_name: str) -> tuple[str, str]:
     """
@@ -24,8 +42,15 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
 
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
-    pass
+    place_name = place_name.replace(' ', '%20')
+    url =f'{MAPBOX_BASE_URL}/{place_name}.json?access_token={MAPBOX_TOKEN}&types=poi'
 
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        response_data = json.loads(response_text)
+
+    longitude, latitude = response_data['features'][0]['center']
+    return latitude,longitude 
 
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
@@ -33,7 +58,22 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    pass
+    mbta_url = f"{MBTA_BASE_URL}?filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance&api_key={MBTA_API_KEY}"
+
+    print(mbta_url)
+
+    with urllib.request.urlopen(mbta_url) as f:
+        response_text = f.read().decode('utf-8')
+        response_data = json.loads(response_text)
+        pprint.pprint(response_data)
+
+        station_name = response_data['data'][0]['attributes']['name']
+        print(station_name)
+
+        wheelchair_accessible = response_data['data'][0]['attributes']['name']
+        print(wheelchair_accessible)
+        return station_name, wheelchair_accessible
+ 
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -44,13 +84,41 @@ def find_stop_near(place_name: str) -> tuple[str, bool]:
     """
     pass
 
+def get_temperature(place_name):
+    """
+    
+    """
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={place_name},us&APPID={WEATHER_APIKEY}&units=metric'
+    with urllib.request.urlopen(url) as f:
+        response_text = f.read().decode('utf-8')
+        response_data = json.loads(response_text)
+    
+    return response_data['main']['temp']
+
+# def find_event(place_name):
+#     """
+    
+#     """
+#     url = f'https://app.ticketmaster.com/discovery/v2/events.json?{TICKET_KEY}'
+#     with urllib.request.urlopen(url) as f:
+#         response_text = f.read().decode('utf-8')
+#         response_data = json.loads(response_text)
+    
+#     return response_data['main']['temp']
+
 
 def main():
     """
     You should test all the above functions here
     """
-    pass
+    place_name = input("Please provide a place name or address: ")
+    print (get_lat_long(place_name)) 
+    longitude, latititude = get_lat_long(place_name)
+    print(get_nearest_station(longitude, latititude))
+    print(get_temperature(place_name))
 
 
 if __name__ == '__main__':
     main()
+
+
