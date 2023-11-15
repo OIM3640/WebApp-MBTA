42e77f8920ede4cea899f7562813300734f2eefb
MaxSun01 <hsun2@babson.edu>
2023-11-15 07:32:19 -0500

    forgot to save

--- templates/app.py
+++ templates/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/search', method = ['POST'])
+def search():
+    place_name = request.form['place_name']
+    stop_name, is_accessible = find_stop_near(place_name)
+    return render_template('result.html', place_name=place_name, stop_name=stop_name, is_accessible=is_accessible)
 
 
 if __name__ == '__main__':
