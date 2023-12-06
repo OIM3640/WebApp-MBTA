9a4abee342762e5deb3ef8e59b7764cce438c28a
Jenny6688 <143571304+Jenny6688@users.noreply.github.com>
2023-12-06 15:18:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def placename():
+    '''
+    1. at http://127.0.0.1:5000/, the user will be greeted by a page that says hello, and includes an input form that requests a place name.
+    2. Upon clicking the 'Submit' button, the data from the form will be sent via a POST request to the Flask backend at the route.
+    3. Then your app will render a mbta_station page for the user - presenting nearest MBTA stop and whether it is wheelchair accessible.
+    4. If something is wrong, the app will render a simple error page
+    '''
     if request.method == 'POST':
         place_name = request.form.get("placename")
         try:
@@ -20,6 +26,8 @@ def placename():
     return render_template("index.html")
 
 def error_page():
+    '''If something is wrong, the app will render a simple error page, which will include some indication that the search did not work, along with a button or link that redirects the user back to the home page.
+    '''
     return render_template("error.html")
 
 # @app.get('/') 
