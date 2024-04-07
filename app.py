eb9f86d5c70b23c4519a1dbe0bc7655530a403e2
Zjasmine1024 <yzhang12@babson.edu>
2024-04-07 15:23:07 -0400

    push

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
-
-
+from app2 import get_coordinates
+from app2 import MAPBOX_TOKEN
+from app2 import find_closest_mbta_stop
+from app2 import MBTA_API_KEY
 app = Flask(__name__)
 
 
@@ -9,5 +11,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/find', methods=['POST'])     # when user send information tp the server, 'find()function will handle it.
+def find():
+    address = request.form['address']
+    try:
+      coordinates = get_coordinates(address, MAPBOX_TOKEN) 
+      lat = coordinates[0]
+      lon = coordinates[1]
+      
+      result = find_closest_mbta_stop(lat, lon, MBTA_API_KEY)
+      
+      
 if __name__ == "__main__":
     app.run(debug=True)
