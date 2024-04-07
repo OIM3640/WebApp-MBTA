8dd741466b36af23de0ef20284584c1b1792db95
XinjiaChen8888 <xchen10@babson.edu>
2024-04-06 20:53:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
-
+from flask import Flask, redirect, render_template, request, url_for
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        place_name = request.form.get('place_name', '')
+        if place_name:  
+            station_name, is_accessible = find_stop_near(place_name)
+            if station_name:
+                return render_template('mbta_station.html', station_name=station_name, is_accessible=is_accessible)
+            else:
+                return render_template('index.html', error_message="No nearest station found in the location you")
+        else:
+            return render_template('index.html', error_message="Please enter a valid place name.")
+    return render_template('index.html')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', error_message='There seems to be an connection issue. Try agin later, or check your internet')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
